9625f7194d8f12479276a495892ece5e6e32e5c0
bmiddour <bcm3620@uncw.edu>
2024-09-26 12:45:00 -0400

    Update Blink_LED.cpp

--- HelloWorld/Blink_LED.cpp
+++ HelloWorld/Blink_LED.cpp
@@ -39,7 +39,43 @@ void loop()
 	// Turn on the LED
 	digitalWrite(MY_LED, HIGH);
 
-	// Leave it on for one second
+	// Leave it on for 1 second (short blink)
+	delay(1s);
+
+	// Turn it off
+	digitalWrite(MY_LED, LOW);
+
+	// Wait one more second
+	delay(1s);
+
+	// Turn on the LED
+	digitalWrite(MY_LED, HIGH);
+	
+	// Leave it on for 1 second (short blink)
+	delay(1s);
+
+	// Turn it off
+	digitalWrite(MY_LED, LOW);
+
+	// Wait one more second
+	delay(1s);
+
+	// Turn on the LED
+	digitalWrite(MY_LED, HIGH);
+
+	// Leave it on for 1 second (short blink)
+	delay(1s);
+
+	// Turn it off
+	digitalWrite(MY_LED, LOW);
+
+	// Wait one more second
+	delay(1s);
+
+	// Turn on the LED
+	digitalWrite(MY_LED, HIGH);
+
+	// Leave it on for 3 second (long blink)
 	delay(3s);
 
 	// Turn it off
@@ -47,6 +83,60 @@ void loop()
 
 	// Wait one more second
 	delay(1s);
+
+	// Turn on the LED
+	digitalWrite(MY_LED, HIGH);
+
+	// Leave it on for 3 second (long blink)
+	delay(3s);
+
+	// Turn it off
+	digitalWrite(MY_LED, LOW);
+
+	// Wait one more second
+	delay(1s);
+
+	// Turn on the LED
+	digitalWrite(MY_LED, HIGH);
+
+	// Leave it on for 3 second (long blink)
+	delay(3s);
+
+	// Turn it off
+	digitalWrite(MY_LED, LOW);
+
+	// Wait one more second
+	delay(1s);
+
+	// Turn on the LED
+	digitalWrite(MY_LED, HIGH);
+
+	// Leave it on for 1 second (short blink)
+	delay(1s);
+
+	// Turn it off
+	digitalWrite(MY_LED, LOW);
+
+	// Wait one more second
+	delay(1s);
+
+	// Leave it on for 1 second (short blink)
+	delay(1s);
+
+	// Turn it off
+	digitalWrite(MY_LED, LOW);
+
+	// Wait one more second
+	delay(1s);
+
+	// Leave it on for 1 second (short blink)
+	delay(1s);
+
+	// Turn it off
+	digitalWrite(MY_LED, LOW);
+
+	// Wait 10 more seconds
+	delay(10s);
 
 	// And repeat!
 }
